8ac86d80dd98f385a3bcf2c31fe8a2cc7d8cd25f
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2024-12-25 14:34:14 +0530

    Backend Update 06 : Update Product.java

--- backend/src/main/java/org/inventrfid/backend/entity/Product.java
+++ backend/src/main/java/org/inventrfid/backend/entity/Product.java
@@ -1,4 +1,32 @@
 package org.inventrfid.backend.entity;
 
+// Product Entity
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pid;
+
+    @Column(name = "productName", nullable = false)
+    private String productName;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
