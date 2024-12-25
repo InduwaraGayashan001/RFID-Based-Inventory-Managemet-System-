515112dcb5d40e91bdd59fb2188aed18f913c79a
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2024-12-25 14:35:54 +0530

    Backend Update 08 : Update ProductRepository.java

--- backend/src/main/java/org/inventrfid/backend/repository/ProductRepository.java
+++ backend/src/main/java/org/inventrfid/backend/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package org.inventrfid.backend.repository;
 
-public class ProductRepository {
+
+
+import org.inventrfid.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    // Additional query methods if needed
 }
