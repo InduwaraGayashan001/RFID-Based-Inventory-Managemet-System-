3db40238a623226fbcf99b02aa95d84c9034890d
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2024-12-25 14:34:58 +0530

    Backend Update 07 : Update ProductDTO.java

--- backend/src/main/java/org/inventrfid/backend/dto/ProductDTO.java
+++ backend/src/main/java/org/inventrfid/backend/dto/ProductDTO.java
@@ -1,4 +1,24 @@
 package org.inventrfid.backend.dto;
 
+// ProductDTO
 public class ProductDTO {
+
+    private int pid;
+    private String productName;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
