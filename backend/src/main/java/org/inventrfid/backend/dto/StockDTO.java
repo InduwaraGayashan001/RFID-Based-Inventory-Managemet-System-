041b073d43ea92ace5db1b837b9e7de47fb989bc
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2024-12-26 13:41:42 +0530

    Backend Update 15 : Update StockDTO.java

--- backend/src/main/java/org/inventrfid/backend/dto/StockDTO.java
+++ backend/src/main/java/org/inventrfid/backend/dto/StockDTO.java
@@ -1,4 +1,54 @@
 package org.inventrfid.backend.dto;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class StockDTO {
+
+    private String rfid;
+    private int productId;
+    private Date timestamp;
+    private int quantity;
+    private BigDecimal stockPrice;
+
+    public String getRfid() {
+        return rfid;
+    }
+
+    public void setRfid(String rfid) {
+        this.rfid = rfid;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getStockPrice() {
+        return stockPrice;
+    }
+
+    public void setStockPrice(BigDecimal stockPrice) {
+        this.stockPrice = stockPrice;
+    }
 }
+
