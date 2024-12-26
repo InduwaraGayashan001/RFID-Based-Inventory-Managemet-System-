50fbcadd9db1079593d75d21c29b08c312bd3aa3
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2024-12-27 00:52:48 +0530

    Backend Update 23 : Update ReleaseRepository.java

--- backend/src/main/java/org/inventrfid/backend/repository/ReleaseRepository.java
+++ backend/src/main/java/org/inventrfid/backend/repository/ReleaseRepository.java
@@ -1,4 +1,10 @@
 package org.inventrfid.backend.repository;
 
-public class ReleaseRepository {
+import org.inventrfid.backend.entity.Release;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReleaseRepository extends JpaRepository<Release, Long> {
+
+    Release findByTransactionId(Long transactionId);
 }
+
